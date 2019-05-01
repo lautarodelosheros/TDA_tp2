84f3297d5bff6ddd9c78f32c26a4cd18501c3379
lautarodelosheros <lautarodelosheros@gmail.com>
2019-05-01 17:25:24 -0300

    Parte 2: Insercion ordenada en vez de merge sort. Salida a sospechosos.txt

--- Parte 2/sospechosos.py
+++ Parte 2/sospechosos.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 from visitante import Visitante
-from merge_sort import merge_sort_por_hora_salida
 
-def generar_sospechosos(visitantes):
-    print('SOSPECHOSOS: ')
+def insertar_ordenado_salidas(salidas, visitante, i = 0):
+    j = len(salidas)
+    while i < j:
+        medio = (i + j) // 2
+        if visitante.hora_salida < salidas[medio].hora_salida:
+            j = medio
+        else:
+            i = medio + 1
+    salidas.insert(i, visitante)
+
+def generar_sospechosos(visitantes, permanencia, archivo):
+    cadena = ''
     for visitante in visitantes:
-        print(visitante.nombre + ',' + str(visitante.permanencia))
+        cadena += visitante.nombre + ','
+    cadena += str(permanencia) + "\n"
+    print("khe")
+    archivo.write(cadena)
 
 def main():
     if len(sys.argv) > 1:
@@ -17,13 +30,16 @@ def main():
         salidas = []
         visitantes = []
 
-        archivo = open(nombre_del_archivo)
-        for linea in archivo.readlines():
+        planilla = open(nombre_del_archivo, "r")
+        for linea in planilla.readlines():
             linea_split = linea.split(',')
             visitante = Visitante(*linea_split)
             entradas.append(visitante)
+        planilla.close()
 
-        archivo.close()
+        if os.path.exists("sospechosos.txt"):
+            os.remove("sospechosos.txt")
+        archivo_salida = open("sospechosos.txt", "w+")
 
         indice_entradas = 0
         indice_salidas = 0
@@ -32,17 +48,20 @@ def main():
             if len(entradas) > indice_entradas and (len(salidas) == 0 or len(salidas) < indice_salidas or entradas[indice_entradas].hora_entrada <= salidas[indice_salidas].hora_salida):
                 visitante = entradas[indice_entradas]
                 visitantes.append(visitante)
-                salidas.append(visitante)
-                merge_sort_por_hora_salida(salidas)
+                insertar_ordenado_salidas(salidas, visitante, indice_salidas)
                 indice_entradas += 1
             # Si es una salida
             else:
                 visitante = salidas[indice_salidas]
-                if 40 <= visitante.hora_salida - visitantes[0].hora_entrada <= 120:
+                duracion = visitante.hora_salida - visitantes[0].hora_entrada
+                print (str(visitante.hora_salida) +"-" + str(visitantes[0].hora_entrada) + "=" + str(duracion))
+                if 40 <= duracion <= 120:
                     if 5 <= len(visitantes) <= 10:
-                        generar_sospechosos(visitantes)
+                        generar_sospechosos(visitantes, duracion, archivo_salida)
                 visitantes.remove(visitante)
                 indice_salidas += 1
+
+        archivo_salida.close()
 
     else:
         print 'Se necesita el nombre del archivo de visitas como parametro'
