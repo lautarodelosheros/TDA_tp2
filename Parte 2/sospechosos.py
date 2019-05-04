885f669610becb75e8d023ad1733907be863dfd0
lautarodelosheros <lautarodelosheros@gmail.com>
2019-05-04 17:00:45 -0300

    Parte 2: Armo la lista de salidas antes así es más eficiente

--- Parte 2/sospechosos.py
+++ Parte 2/sospechosos.py
@@ -27,8 +27,7 @@ def generar_sospechosos(visitantes, permanencia, archivo):
     cadena += str(permanencia) + "\n"
     archivo.write(cadena)
 
-def investigar(entradas, archivo_salida):
-    salidas = []
+def investigar(entradas, salidas, archivo_salida):
     visitantes = []
     indice_entradas = 0
     indice_salidas = 0
@@ -37,7 +36,6 @@ def investigar(entradas, archivo_salida):
         if len(entradas) > indice_entradas and (len(salidas) == 0 or len(salidas) < indice_salidas or entradas[indice_entradas].hora_entrada <= salidas[indice_salidas].hora_salida):
             visitante = entradas[indice_entradas]
             visitantes.append(visitante)
-            insertar_ordenado_salidas(salidas, visitante, indice_salidas)
             indice_entradas += 1
         # Si es una salida
         else:
@@ -54,19 +52,21 @@ def main():
         nombre_del_archivo = sys.argv[1]
 
         entradas = []
+        salidas = []
 
         planilla = open(nombre_del_archivo, "r")
         for linea in planilla.readlines():
             linea_split = linea.split(',')
             visitante = Visitante(*linea_split)
             entradas.append(visitante)
+            insertar_ordenado_salidas(salidas, visitante)
         planilla.close()
 
         if os.path.exists("sospechosos.txt"):
             os.remove("sospechosos.txt")
         archivo_salida = open("sospechosos.txt", "w+")
 
-        investigar(entradas, archivo_salida)
+        investigar(entradas, salidas, archivo_salida)
 
         archivo_salida.close()
 
